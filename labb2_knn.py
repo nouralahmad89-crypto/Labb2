44eccb4b10d52aed29a564c61a8a56ffd019dd73
Nour <nouralahmad89@gmail.com>
2025-09-26 12:47:55 +0200

    handel exceptions

--- labb2_knn.py
+++ labb2_knn.py
@@ -1,5 +1,5 @@
 # Labb 2: Klassificering av Pichu och Pikachu med KNN
-# importera bibliotek och create .venv file
+# importera bibliotek och skapa .venv file
 import matplotlib.pyplot as plt
 import math
 import numpy as np
@@ -7,59 +7,87 @@ import re
 
 # läs in data från datapoints.txt fil 
 dimansion=[] # en lista för att spara width och height
-Labl= [] # en lista för att spara labels
-with open("datapoints.txt" ,"r") as f:
+labl= [] # en lista för att spara labels
+try:
+   with open("datapoints.txt" ,"r") as f:
     f.readline()  # hoppa över första raden (header)
     for line in f:
         parts= line.strip().split(",")
         width= float (parts[0])
         height= float(parts[1])
-        Label= float(parts[2])
+        label= float(parts[2])
         dimansion.append([width , height])
-        Labl.append(Label)       
-        #print(dimansion)
+        labl.append(label)       
+        #print(dimansion) check om alla ser ut bra
         #print(Labl)
+except FileNotFoundError:
+    raise FileNotFoundError ("File not found, make sure path is correct!")        
 dimansion = np.array(dimansion) # konvertera till numpy-arrays
-Labl = np.array(Labl)
+labl = np.array(labl)
 # Plotta data 
 plt.figure(figsize=(7,5))
-pichu= np.where(Labl==0) # hämta index för pichu.
-pikachu = np.where(Labl == 1) # hämta index för pikachu.
+pichu= np.where(labl==0) # hämta index för pichu.
+pikachu = np.where(labl == 1) # hämta index för pikachu.
 plt.scatter(dimansion[pichu, 0], dimansion[pichu, 1],
             color="blue", label="Pichu (0)", s=30, alpha=0.5) # plottar pichu punkter i blått
 plt.scatter(dimansion[pikachu, 0], dimansion[pikachu, 1],
             color="yellow", label="Pikachu (1)", s=60, alpha=0.7) # plottar pikachu punkter i gult
-plt.xlabel("Width (cm)")
-plt.ylabel("Height (cm)")
+plt.xlabel("Width")
+plt.ylabel("Height")
 plt.title("Pichu vs Pikachu - Data Points")
 plt.legend()
 plt.grid(True)
 plt.show()
 # läs in test punnkter
 testpoints=[]
-with open("testpoints.txt", "r") as test:
-    test.readline()
-    for T in test:
+try:
+    with open("testpoints.txt", "r") as test:
+     test.readline()
+     for T in test:
         T= T.strip()
-        point= T.split("(")[1].split(")")[0]
-        x,y= map(float, point.split(","))
-        testpoints.append([x,y])   
-    
+        point= T.split("(")[1].split(")")[0] 
+        x,y= point.split(",")
+        testpoints.append([x,y])
+except FileNotFoundError:   
+       raise FileNotFoundError("File not found, make sure path is correct!")     
 # en funktion för att beräkna avståndet mellan punkter
 def euclidean(a, b):
      return np.sqrt(np.power(a[0]-b[0], 2) + np.power(a[1]-b[1], 2))
 for pt in testpoints: # loopa igenom testpunkterna och klassificera
     try:
-        if pt[0]<0 or pt[1]<0:  # Kontrollera negativa värden
-            raise ValueError(f"punkten{pt[0]}{pt[1]} inhåller negativa väredn")
-        distances = [euclidean(pt, x) for x in dimansion]
-        nearest_index = np.argmin(distances)      # index för närmaste punkt
-        predicted_label = Labl[nearest_index]     # hämta label för den närmaste
-        if predicted_label == 1:
-            print(f"Sample with (width, height): ({pt[0]}, {pt[1]}) classified as Pikachu")
-        else:
-            print(f"Sample with (width, height): ({pt[0]}, {pt[1]}) classified as Pichu")
-    except ValueError as err:  
-        print(err)
-        print(f"Fel: punkten ({pt[0]}, {pt[1]}) innehåller icke-numeriska värden, hoppar över")
-        continue
+        # covert till float
+        pt[0]= float(pt[0]) 
+        pt[1]= float(pt[1])
+    except ValueError :  
+             print(f"Fel: punkten ({pt[0]}, {pt[1]}) innehåller icke-numeriska värden, hoppar över")
+             continue # Hoppa över den här punkten               
+    if pt[0]<0 or pt[1]<0:  # Kontrollera negativa värden
+        print(f"punkten{pt[0]},{pt[1]} inhåller negativa väredn, hpoppa över")
+        continue # hoppa över den här punkten
+    # Beräkna avstånd och klassificera   
+    distances = [euclidean(pt, x) for x in dimansion]
+    nearest_index = np.argmin(distances)      # index för närmaste punkt
+    predicted_label = labl[nearest_index]     # hämta label för den närmaste
+    if predicted_label == 1:
+        print(f"Sample with (width, height): ({pt[0]}, {pt[1]}) classified as Pikachu")
+    else:
+        print(f"Sample with (width, height): ({pt[0]}, {pt[1]}) classified as Pichu")
+  # 10-NN (k=10)
+def classify_knn (testpoints, dimansion,labl, k=10):
+    resluts=[] # spara all resluts from Euclidean methon
+    for p in testpoints:
+         # Kontrollera negativa eller icke-numeriska värden
+        try:
+            x, y = float(p[0]), float(p[1])
+        except ValueError:
+            print(f"hoppa över icke-numeriska vären({p[0]}, {p[1]})")
+            continue    
+        if x < 0 or y < 0:
+            print(f" hoppa över negativa väredn: ({x}, {y})")
+            continue
+        for x in dimansion:
+         resluts=resluts.append(euclidean(p,x))
+    index_sort= np.argsort(resluts)[:k]
+    
+
+
